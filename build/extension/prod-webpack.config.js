7b91c12f4791ae19b69dd80f5bd147226a3a54b9
WAY24145 <wassiou.ayedoun@open-groupe.com>
2025-02-11 10:49:16 +0100

    (fix/mapstore)mise à jour du prod-webpack.config.js(cdcrm-1545)

--- build/extension/prod-webpack.config.js
+++ build/extension/prod-webpack.config.js
@@ -7,12 +7,13 @@ const ZipPlugin = require('zip-webpack-plugin');
 const {name} = require('../../config');
 const { plugins: commonsPlugins, ...commons} = require('./commons');
 
-// the build configuration for production allow to create the final zip file, compressed accordingly
 const plugins = [
-    new CopyPlugin([
-        { from: path.resolve(__dirname, "..", "..", "assets", "translations"), to: "translations" },
-        { from: path.resolve(__dirname, "..", "..", "assets", "index.json"), to: "index.json" }
-    ]),
+    new CopyPlugin({
+        patterns: [
+            { from: path.resolve(__dirname, "..", "..", "assets", "translations"), to: "translations" },
+            { from: path.resolve(__dirname, "..", "..", "assets", "index.json"), to: "index.json" }
+        ]
+    }),
     new ZipPlugin({
         filename: `${name}.zip`,
         pathMapper: assetPath => {
@@ -21,10 +22,7 @@ const plugins = [
             }
             // other files have to be placed in the root, with the same name
             return path.basename(assetPath);
-        }
-    }),
-    ...commonsPlugins
-];
+        }    }),    ...commonsPlugins];
 
 // Temporary TODO: Has to be updated in createExtensionWebpackConfig
 const fileLoader = {
